0735bdc3b19582505bcf625a9789cde6cd3cd21d
Denis <denmagg.work@gmail.com>
2022-09-01 20:11:31 +0300

    added delegate to open preview from the searchbar

--- NetflixAPP/Controllers/Core/SearchViewController.swift
+++ NetflixAPP/Controllers/Core/SearchViewController.swift
@@ -85,7 +85,33 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     
 }
 
-extension SearchViewController: UISearchResultsUpdating {
+func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    
+    let title = titles[indexPath.row]
+    
+    guard let titleName = title.original_title ?? title.original_name else {
+        return
+    }
+    
+    
+    APICaller.shared.getMovie(with: titleName) { [weak self] result in
+        switch result {
+        case .success(let videoElement):
+            DispatchQueue.main.async {
+                let vc = TitlePreviewViewController()
+                vc.configure(with: TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: title.overview ?? ""))
+                self?.navigationController?.pushViewController(vc, animated: true)
+            }
+            
+            
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating, SearchResultsViewControllerDelegate {
     
     func updateSearchResults(for searchController: UISearchController) {
         let searchBar = searchController.searchBar
@@ -94,6 +120,7 @@ extension SearchViewController: UISearchResultsUpdating {
               !query.trimmingCharacters(in: .whitespaces).isEmpty,
               query.trimmingCharacters(in: .whitespaces).count >= 3,
               let resultsController = searchController.searchResultsController as? SearchResultsViewController else { return }
+        resultsController.delegate = self
         
         APICaller.shared.search(with: query) { result in
             DispatchQueue.main.async {
@@ -105,6 +132,14 @@ extension SearchViewController: UISearchResultsUpdating {
                     print(error.localizedDescription)
                 }
             }
+        }
+    }
+    
+    func searchResultsViewControllerDidTapItem(_ viewModel: TitlePreviewViewModel) {
+        DispatchQueue.main.async { [weak self] in
+            let vc = TitlePreviewViewController()
+            vc.configure(with: viewModel)
+            self?.navigationController?.pushViewController(vc, animated: true)
         }
     }
 }
