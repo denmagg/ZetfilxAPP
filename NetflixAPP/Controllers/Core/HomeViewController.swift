7a4982f6a45a0d25d18841d22ca8ea9f85c63f62
Denis <denmagg.work@gmail.com>
2022-09-01 19:18:25 +0300

    added randomMovie to the header of homeVC

--- NetflixAPP/Controllers/Core/HomeViewController.swift
+++ NetflixAPP/Controllers/Core/HomeViewController.swift
@@ -18,6 +18,8 @@ enum Sections: Int {
 
 class HomeViewController: UIViewController {
     
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
     
     let sectionTitles: [String] = ["Trending Movies", "Trending Tv", "Popular", "Upcoming Movies", "Top rated"]
     
@@ -40,7 +42,26 @@ class HomeViewController: UIViewController {
         
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
+        configureHeroHeaderView()
                 
+    }
+    
+    private func configureHeroHeaderView() {
+
+        APICaller.shared.getTrendingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                
+                self?.randomTrendingMovie = selectedTitle
+                self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+            case .failure(let erorr):
+                print(erorr.localizedDescription)
+            }
+        }
+        
+
+
     }
     
     private func configureNavbar() {
